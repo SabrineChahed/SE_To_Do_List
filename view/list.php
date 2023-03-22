5fe2f37b714ad8a4f88f6d6e83b10eb9c0dcd998
Nassiima <125696068+Nassiima@users.noreply.github.com>
2023-03-22 09:40:36 +0100

    Update list.php

--- view/list.php
+++ view/list.php
@@ -1,4 +1,18 @@
-    <form action="index.php" method="get">      
+
+<div class="col shadow main-col bg-white">
+      <div class="row bg-primary text-white">
+        <div class="col  p-2">
+          <h4><?php echo LANG_APP_NAME;?></h4>
+        </div>
+        <div class="col">
+             <div class="float-right p-2" >
+                <a href="index.php?action=logout"><b style="color:white;">Logout</b></a>
+            </div>
+        </div>
+      </div>
+    
+    
+<form action="index.php" method="get">      
       <input type="hidden" name="action" value="do_add"    />
       <div class="row justify-content-between text-white p-2">
         
